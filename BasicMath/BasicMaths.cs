70ecdbaf48723210930ee8d55817cc390e756d32
Long Duong <duongthanhlong220805@gmail.com>
2025-05-24 15:40:12 +0700

    update CalculatorCsvTest

--- BasicMath/BasicMaths.cs
+++ BasicMath/BasicMaths.cs
@@ -31,4 +31,29 @@ namespace BasicMath
             return num1 * num2;
         }
     }
+    public class Calculator
+    {
+        public Calculator()
+        {
+            // Constructor logic here
+        }
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Divide(int num1, int num2)
+        {
+            if (num2 == 0)
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            return num1 / num2;
+        }
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
 }
